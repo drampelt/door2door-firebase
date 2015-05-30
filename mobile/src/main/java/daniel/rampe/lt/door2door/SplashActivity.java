9edda653ca34693b7b591525445447987e8aa58d
Daniel Rampelt <drampelt@gmail.com>
2015-05-30 13:58:02 -0400

    login

--- mobile/src/main/java/daniel/rampe/lt/door2door/SplashActivity.java
+++ mobile/src/main/java/daniel/rampe/lt/door2door/SplashActivity.java
@@ -1,7 +1,9 @@
 package daniel.rampe.lt.door2door;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -30,144 +32,33 @@ import org.androidannotations.annotations.ViewById;
 import java.io.IOException;
 import java.util.Map;
 
+import daniel.rampe.lt.door2door.activities.LoginActivity_;
+
 @EActivity(R.layout.activity_splash)
-public class SplashActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class SplashActivity extends AppCompatActivity {
     private static final String LOG_TAG = SplashActivity.class.getSimpleName();
+
+    private SharedPreferences mPreferences;
+
     private Firebase mFirebase;
 
-    private GoogleApiClient mGoogleApiClient;
-    private boolean mGoogleIntentInProgress = false;
-    private ConnectionResult mGoogleConnectionResult;
-    private boolean mGoogleLoginClicked = false;
-    public static final int RC_GOOGLE_LOGIN = 1;
-
-    @ViewById(R.id.google_login_button)
-    SignInButton mGoogleLoginButton;
-
     @AfterViews
-    void checkLogin() {
+    void init() {
         mFirebase = Door2Door.getFirebase();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks(this)
-//                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API)
-                .addScope(Plus.SCOPE_PLUS_LOGIN)
-                .build();
-        if(mFirebase.getAuth() != null) {
-            Log.d(LOG_TAG, "We have stuff");
-        } else {
-            Log.d(LOG_TAG, "We don't have stuff");
-//            googleLogin();
+        mPreferences = getPreferences(Context.MODE_PRIVATE);
+        if(mPreferences.contains("email") && mPreferences.contains("password")) {
+            Log.d(LOG_TAG, "TODO auto redirect");
         }
     }
 
-    @Click(R.id.google_login_button)
-    void onClickGoogleSigninButton() {
-        mGoogleLoginClicked = true;
-        if(!mGoogleApiClient.isConnecting()) {
-            if(mGoogleConnectionResult != null) {
-                resolveSignInError();
-            } else if(mGoogleApiClient.isConnected()) {
-                googleLogin();
-            } else {
-                Log.d(LOG_TAG, "Trying to connect to Google API");
-                mGoogleApiClient.connect();
-            }
-        }
+    @Click(R.id.login_button)
+    void onClickLogin() {
+        LoginActivity_.intent(this).start();
     }
 
-    private void showErrorDialog(String message) {
-        new AlertDialog.Builder(this)
-                .setTitle("Error")
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok, null)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
+    @Click(R.id.register_button)
+    void onClickRegister() {
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_GOOGLE_LOGIN) {
-            if(resultCode != RESULT_OK) {
-                Log.d(LOG_TAG, "something bad happened");
-            }
-            mGoogleIntentInProgress = false;
-            if(!mGoogleApiClient.isConnecting()) {
-                mGoogleApiClient.connect();
-            }
-        }
-    }
-
-    @Background
-    void googleLogin() {
-        String token = null;
-        try {
-            String scope = String.format("oauth2:%s", Scopes.PLUS_LOGIN);
-            token = GoogleAuthUtil.getToken(this, Plus.AccountApi.getAccountName(mGoogleApiClient), scope);
-        } catch (IOException ex) {
-            Log.e(LOG_TAG, "Error", ex);
-        } catch (UserRecoverableAuthException ex) {
-            Log.w(LOG_TAG, "Recoverable error" + ex.toString());
-            // TODO: check if we are doing this already
-            if(!mGoogleIntentInProgress) {
-                Intent recover = ex.getIntent();
-                startActivityForResult(recover, RC_GOOGLE_LOGIN);
-                mGoogleIntentInProgress = true;
-            }
-        } catch (GoogleAuthException ex) {
-            Log.e(LOG_TAG, "Error authenticating " + ex.toString());
-        }
-
-        if(token != null) {
-            mFirebase.authWithOAuthToken("google", token, new Firebase.AuthResultHandler() {
-                @Override
-                public void onAuthenticated(AuthData authData) {
-                    MainActivity_.intent(SplashActivity.this).start();
-                }
-
-                @Override
-                public void onAuthenticationError(FirebaseError firebaseError) {
-                    Log.e(LOG_TAG, "Authentication error:  google login failed.");
-                }
-            });
-        }
-    }
-
-    private void resolveSignInError() {
-        if(mGoogleConnectionResult.hasResolution()) {
-            try {
-                mGoogleIntentInProgress = true;
-                mGoogleConnectionResult.startResolutionForResult(this, RC_GOOGLE_LOGIN);
-            } catch (IntentSender.SendIntentException e) {
-                mGoogleIntentInProgress = false;
-                mGoogleApiClient.connect();
-            }
-        }
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.d(LOG_TAG, "connected");
-        googleLogin();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(LOG_TAG, "connection suspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(LOG_TAG, "connection failed");
-        if(!mGoogleIntentInProgress) {
-            mGoogleConnectionResult = connectionResult;
-
-            if(mGoogleLoginClicked) {
-                resolveSignInError();
-            } else {
-                Log.e(LOG_TAG, connectionResult.toString());
-            }
-        }
-    }
 }
---
+++ mobile/src/main/java/daniel/rampe/lt/door2door/activities/LoginActivity.java
@@ -0,0 +1,82 @@
+package daniel.rampe.lt.door2door.activities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.ViewById;
+
+import daniel.rampe.lt.door2door.Door2Door;
+import daniel.rampe.lt.door2door.R;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+
+    private Firebase mFirebase;
+    private SharedPreferences mPreferences;
+
+    @ViewById(R.id.email)
+    EditText mEmail;
+
+    @ViewById(R.id.password)
+    EditText mPassword;
+
+
+    @AfterViews
+    void init() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mFirebase = Door2Door.getFirebase();
+        mPreferences = getPreferences(Context.MODE_PRIVATE);
+        if(mPreferences.contains("email") && mPreferences.contains("password")) {
+            login(mPreferences.getString("email", null), mPreferences.getString("password", null));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
+
+    @Click(R.id.login_button)
+    void onClickLogin() {
+        Log.d(LOG_TAG, "Click login");
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill in the fields", Toast.LENGTH_LONG).show();
+        } else {
+            login(email, password);
+        }
+    }
+
+    private void login(String email, String password) {
+        mFirebase.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                Log.d(LOG_TAG, "we have authenticated");
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                Log.e(LOG_TAG, "Error authenticating: " + firebaseError.toString());
+            }
+        });
+    }
+
+}
